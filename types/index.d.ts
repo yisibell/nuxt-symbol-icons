fde26e5a211ae9f8731b6d220f49cdc0024529d5
wenqing <wenqing@kerrylan.com>
2023-03-29 11:50:55 +0800

    feat(types): add types

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+
+interface NuxtSymbolIconsOptions {
+  enable?: boolean
+  svgSymbolIdPrefix?: string
+}
+
+declare const NuxtSymbolIconsModule: (moduleOptions?: NuxtSymbolIconsOptions) => void
+
+export default NuxtSymbolIconsModule
