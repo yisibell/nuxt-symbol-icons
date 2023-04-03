3ade37539cc3f1ad84663bb654a309f9b9528209
wenqing <wenqing@kerrylan.com>
2023-04-03 11:52:03 +0800

    chore(release): v1.7.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,17 @@
 # Changelog
 
 
+## v1.7.0
+
+
+### 🚀 Enhancements
+
+  - Support svgSpriteLoaderIncludeDir and requireContextSvgDir (f087cd9)
+
+### ❤️  Contributors
+
+- Wenqing <wenqing@kerrylan.com>
+
 ## v1.6.4
 
 
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,8 @@ interface NuxtSymbolIconsOptions {
   enable?: boolean
   svgSymbolIdPrefix?: string
   globalComponentName?: string
+  requireContextSvgDir?: string
+  svgSpriteLoaderIncludeDir?: string
 }
 
 declare const NuxtSymbolIconsModule: (
